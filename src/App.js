a6380df5adad86ad2f15055293aa61c267d6bd30
aroble <abdulladif0042@gmail.com>
2023-01-27 09:44:40 +0300

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Header";
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Home from "./Pages/Home"
+import Blog from "./Pages/Blog";
+import Dashboard from "./Pages/Dashboard";
+import New from "./Pages/New"
+import Profile from "./Pages/Profile"
+import ChangePassword from "./Pages/ChangePassword";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div >
+      <BrowserRouter>
+      <Header/>
+      <div className="px-20  m-auto">
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/blog" element={<Blog/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/new" element={<New/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/change" element={<ChangePassword/>}/>
+        </Routes>
+        </div>
+      </BrowserRouter>
+
+   </div>
   );
 }
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,31 @@
+import BlogCard from "../Components/BlogCard"
+import SideBar from "../Components/SideBar"
+import ListCard from "../Components/ListCard"
+
+function Home(){
+    return (
+        <div className="flex justify-between space-x-5 mt-5">
+           <SideBar/>
+            <div className="flex-1">
+                <BlogCard/>
+                <BlogCard/>
+            </div>
+            <div className="basis-1/4">
+                <div className="bg-slate-50 py-2 rounded-md">
+                    <div className="flex justify-between items-center px-5 py-2">
+                        <h3 className="font-bold">Listings</h3>
+                        <small className="text-blue-700 font-semibold">See all</small>
+                    </div>
+                    <ListCard/>
+                    <ListCard/>
+                    <ListCard/>
+                    <ListCard/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
+
+
